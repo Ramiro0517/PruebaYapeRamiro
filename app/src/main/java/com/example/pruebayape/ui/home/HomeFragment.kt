c681ef874937027a6e323b543fe5c8b8c72660ab
Ramiro0517 <89993380+Ramiro0517@users.noreply.github.com>
2024-06-20 03:01:05 -0500

    initial commit

---
+++ app/src/main/java/com/example/pruebayape/ui/home/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.example.pruebayape.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.pruebayape.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/pruebayape/ui/map/MapFragment.kt
@@ -0,0 +1,26 @@
+package com.example.pruebayape.ui.map
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.pruebayape.R
+import com.example.pruebayape.databinding.FragmentMapBinding
+
+
+class MapFragment : Fragment() {
+
+    private var _binding: FragmentMapBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMapBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+
+}
